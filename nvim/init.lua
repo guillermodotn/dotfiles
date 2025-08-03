a9da0ae2095cc39eec335e529fb841b8af91fcf4
guillermodotn <guillerm0.n@outlook.es>
2025-08-03 14:01:11 +0200

    Move nvim config from DSL to lua.

---
+++ nvim/init.lua
@@ -0,0 +1,28 @@
+-- Set leader key
+vim.g.mapleader = " "
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tab & indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Mouse support
+vim.opt.mouse = "a"
+
+-- Clipboard integration
+vim.opt.clipboard = "unnamedplus"
+
+-- Keybindings example: jj to escape insert mode
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- Set cursor column
+vim.opt.cursorcolumn = true
+
