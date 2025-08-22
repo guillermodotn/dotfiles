941c4817dae2582f28d40730920b2d133e0df68f
guillermodotn <guillerm0.n@outlook.es>
2025-08-22 22:38:37 +0200

    Add env-vars file.

---
+++ bashrc.d/env-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Check for nvim, vim and vi and set the default editor.
+if command -v nvim &> /dev/null; then
+    export EDITOR=nvim
+elif command -v vim &> /dev/null; then
+    export EDITOR=vim
+elif command -v vi &> /dev/null; then
+    export EDITOR=vi
+fi
