07bc448000ea447abfde77439a74e59c1abb0a76
guillermodotn <guillerm0.n@outlook.es>
2025-10-01 10:33:09 +0200

    Tab names switch

--- bashrc.d/tmux-auto-init.sh
+++ bashrc.d/tmux-auto-init.sh
@@ -2,7 +2,7 @@
 
 # Tmux auto-attach/create
 if command -v tmux &> /dev/null && [ -z "$TMUX" ] && [ -z "$ENV_VAR" ]; then
-    tmux attach -t main || tmux new -s main \; new-window -n $(basename $EDITOR)
+	tmux attach -t main || tmux new -s main -n $(basename $EDITOR) -c $EDITOR \; new-window
 elif command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-    tmux attach -t main || tmux new -s main
+	tmux attach -t main || tmux new -s main
 fi
