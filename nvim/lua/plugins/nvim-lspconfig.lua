3da0f767202ce7dffb6d69703955ce2ca8d55b47
guillermodotn <guillerm0.n@outlook.es>
2025-12-19 21:11:19 +0100

    nvim: jinja lsp

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -213,6 +213,7 @@ return {
 			rpm_lsp_server = {},
 			docker_language_server = {},
 			rust_analyzer = {},
+			jinja_lsp = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
 			-- Some languages (like typescript) have entire language plugins that can be useful:
