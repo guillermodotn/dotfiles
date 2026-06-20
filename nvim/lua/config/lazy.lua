72bff9356de41a1c5d2ead63ad09b69b6f4cd3f4
guillermodotn <guillerm0.n@outlook.es>
2026-06-20 02:10:10 +0200

    nvim: dropping lazy-lock.json

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
 	-- spec_dir = vim.fn.stdpath("config") .. "/lua/plugins",
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "earthsong" } },
+	install = { missing = true, colorscheme = { "earthsong" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 	ui = {
