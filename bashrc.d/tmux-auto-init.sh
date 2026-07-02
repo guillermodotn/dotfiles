ffc1160d618f733d14672068f59cde7b46fad016
guillermodotn <guillerm0.n@outlook.es>
2026-07-02 17:29:00 +0200

    bashrc.d simplification

--- bashrc.d/tmux-auto-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# Tmux auto-attach/create
-if command -v tmux &> /dev/null && [ -z "$TMUX" ] && [ -z "$ENV_VAR" ]; then
-	tmux attach -t main || tmux new -s main -n $(basename $EDITOR) -c $EDITOR \; new-window
-elif command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-	tmux attach -t main || tmux new -s main
-fi
